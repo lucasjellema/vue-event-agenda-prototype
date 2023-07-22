65ea732d0e88e59255f00a1362232722578ff05d
Lucas Jellema <lucasjellema@gmail.com>
2023-07-22 11:27:17 +0000

    Rename AboutView to CalendarView

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,12 +11,12 @@ const router = createRouter({
       component: EventListView
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/calendar',
+      name: 'calendar',
       // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
+      // this generates a separate chunk (Calendar.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/CalendarView.vue')
     }
   ]
 })
